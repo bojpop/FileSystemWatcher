98352f839cfb02f89dedea0c9e98ca9ede56ef7b
bojpop <poppfc@gmail.com>
2016-03-06 11:50:11 +0100

    no message

---
+++ FileSystemWatcher/PaymentFileProcessor.cs
@@ -0,0 +1,14 @@
+﻿using MailFunctionality;
+
+namespace FileSystemWatcher
+{
+    internal class PaymentFileProcessor
+    {
+        public void ProcessFileFrom(string path)
+        {
+            Functions.Parsing(path);
+            Functions.SendMail("Info mail", path);
+            Functions.CopyFile(path);
+        }
+    }
+}
